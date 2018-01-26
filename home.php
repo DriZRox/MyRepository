b7d10186fe384554d7c0a8e4a8acf5d9c9e32337
24thsaint <admin@blackout.biz.tm>
2018-01-26 23:17:23 +0800

    Feature: Sidebar implemented

--- home.php
+++ home.php
@@ -3,5 +3,17 @@
 </head>
 <body>
 <h1 align="center">This is the home file!</h1>
+
+<sidebar>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+  <a href="item">Item</a>
+</sidebar>
+
 </body>
 </html>
