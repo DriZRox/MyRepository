ae6242c88d278f00ba247b1126a9f76a1be11583
Rodrigo <games_viciados@hotmail.com>
2018-01-26 15:21:44 +0000

    Edited sidebar on home.php

--- home.php
+++ home.php
@@ -5,14 +5,15 @@
 <h1 align="center">This is the home file!</h1>
 
 <sidebar>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
-  <a href="item">Item</a>
+  <a href="item">Item 1</a>
+  <a href="item">Item 2</a>
+  <a href="item">Item 3</a>
+  <a href="item">Item 4</a>
+  <a href="item">Item 5</a>
+  <a href="item">Item 6<a>
+  <a href="item">Item 7</a>
+  <a href="item">Item 8</a>
+  <a href="login">Login</a>
 </sidebar>
 
 </body>
